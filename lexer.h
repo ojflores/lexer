eb4f300a774e5857711f89d70bed12d9aaf1d004
ojflores <ojflores@puc.edu>
2017-11-06 01:51:10 +0000

    first commit

---
+++ lexer.h
@@ -0,0 +1,24 @@
+#ifndef LEXER_H
+#define LEXER_H
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+using namespace std;
+
+class Lexer{
+	private:
+	
+	public:
+		
+		
+		//constructor
+		Lexer();
+		
+		//takes the input and finds what token it is classified as
+		void GetToken();
+	
+	
+};
+
+#endif
